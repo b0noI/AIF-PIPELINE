e2ec3a274786248028b9f94a759a3e902982de5c
Nazeem <ifthen@amazon.com>
2015-06-13 10:53:59 +0100

    Added charset recognition.

--- src/main/java/io/aif/pipeline/fabric/plain/FileTextFactory.java
+++ src/main/java/io/aif/pipeline/fabric/plain/FileTextFactory.java
@@ -6,20 +6,35 @@ import io.aif.pipeline.model.ISemanticText;
 import io.aif.pipeline.model.IText;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
 
 public class FileTextFactory extends AbstractTextFactory {
 
     private final Path path;
+    private final Charset charset;
 
     public FileTextFactory(final Path path) {
         this.path = path;
+        this.charset = Charset.forName("UTF-8");
+    }
+
+    public FileTextFactory(final Path path, Charset charset) {
+        this.path = path;
+        this.charset = charset;
     }
 
     public static void main(final String[] args) {
         final Path path = Paths.get(args[0]);
-        final FileTextFactory fileTextFactory = new FileTextFactory(path);
+        final FileTextFactory fileTextFactory;
+        if (args.length > 1) {
+            final Charset encoding = Charset.forName(args[1]);
+            fileTextFactory = new FileTextFactory(path, encoding);
+        } else {
+            fileTextFactory = new FileTextFactory(path);
+        }
         System.out.println("Building TEXT");
         final IText text = fileTextFactory.build();
         System.out.println("Building semanticText");
@@ -31,15 +46,15 @@ public class FileTextFactory extends AbstractTextFactory {
     @Override
     protected String getRawText() {
         try {
-            return readAllText(new FileInputStream(path.toFile()));
+            return readAllText(new FileInputStream(path.toFile()), charset);
         } catch (IOException e) {
             e.printStackTrace();
             return "";
         }
     }
 
-    public static String readAllText(final InputStream inputStream) throws IOException {
-        try(final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+    public static String readAllText(final InputStream inputStream, Charset charset) throws IOException {
+        try(final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset))) {
 
             final StringBuffer buff = new StringBuffer();
 
